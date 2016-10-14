a18d8e0a68482b8d5fd56ba7d0fef0403db5dbfc
王鑫鹏 <448932081@qq.com>
2016-10-14 14:46:47 +0800

    更改项目图片，添加闪屏Activity（未写代码） 2016/10/14-14:46

--- app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
@@ -1,13 +1,15 @@
-package cn.ucai.fulicenter;
+package cn.ucai.fulicenter.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import cn.ucai.fulicenter.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
