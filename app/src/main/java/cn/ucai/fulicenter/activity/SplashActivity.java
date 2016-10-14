90dc3edfda5eaa213abcfc6744e7923e277ebfc2
王鑫鹏 <448932081@qq.com>
2016-10-14 15:19:57 +0800

    添加闪屏的代码 2016/10/14-15:20

--- app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
@@ -1,15 +1,40 @@
 package cn.ucai.fulicenter.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import cn.ucai.fulicenter.R;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final int sleepTime = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long start =System.currentTimeMillis();
+                long costTime =System.currentTimeMillis()-start;
+                //等待sleeptime时间
+                if (sleepTime-costTime>0){
+                    try {
+                        Thread.sleep(sleepTime-costTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                //进入主页面
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        }).start();
     }
 }
