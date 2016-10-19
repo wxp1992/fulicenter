a1390aa3930e18ad36ec4a45aa2a8f37c4001dd4
王鑫鹏 <448932081@qq.com>
2016-10-19 13:01:23 +0800

    写下载精品首页的代码

--- app/src/main/java/cn/ucai/fulicenter/utils/NetDao.java
+++ app/src/main/java/cn/ucai/fulicenter/utils/NetDao.java
@@ -3,6 +3,7 @@ package cn.ucai.fulicenter.utils;
 import android.content.Context;
 
 import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.bean.BoutiqueBean;
 import cn.ucai.fulicenter.bean.GoodsDetailsBean;
 import cn.ucai.fulicenter.bean.NewGoodsBean;
 
@@ -26,6 +27,12 @@ public class NetDao {
         utils.setRequestUrl(I.REQUEST_FIND_GOOD_DETAILS)
                 .addParam(I.GoodsDetails.KEY_GOODS_ID, String.valueOf(goodsId))
                 .targetClass(GoodsDetailsBean.class)
+                .execute(listener);
+    }
+    public static void downloadBoutique(Context context, OkHttpUtils.OnCompleteListener<BoutiqueBean[]> listener) {
+        OkHttpUtils utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_FIND_BOUTIQUES)
+                .targetClass(BoutiqueBean[].class)
                 .execute(listener);
     }
     }
