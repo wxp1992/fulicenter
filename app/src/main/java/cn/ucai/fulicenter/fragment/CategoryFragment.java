9152f525448fdd0bdc014594f8687c6b269a762a
王鑫鹏 <448932081@qq.com>
2016-10-20 14:34:40 +0800

    大分类和小分类的内容对应

--- app/src/main/java/cn/ucai/fulicenter/fragment/CategoryFragment.java
+++ app/src/main/java/cn/ucai/fulicenter/fragment/CategoryFragment.java
@@ -26,7 +26,7 @@ import cn.ucai.fulicenter.utils.OkHttpUtils;
  */
 
 public class CategoryFragment extends BaseFragment {
-    private  static  final  String TAG=CategoryFragment.class.getSimpleName();
+//    private  static  final  String TAG=CategoryFragment.class.getSimpleName();
 
     @BindView(R.id.elv_category)
     ExpandableListView mElvCategory;
@@ -70,13 +70,15 @@ public class CategoryFragment extends BaseFragment {
                     ArrayList<CategoryGroupBean> groupList = ConvertUtils.array2List(result);
                     L.e("groupList=" + groupList.size());
                     mGroupList.addAll(groupList);
-                    for (CategoryGroupBean g : groupList) {
-                        downloadChild(g.getId());
+                    for (int i=0;i<groupList.size();i++) {
+                        mChildList.add(new ArrayList<CategoryChildBean>());
+                        CategoryGroupBean g = groupList.get(i);
+                        downloadChild(g.getId(),i);
                     }
                 }
             }
 
-            private void downloadChild(int id) {
+            private void downloadChild(int id,final int index) {
                 NetDao.downloadCategoryChild(mContext, id, new OkHttpUtils.OnCompleteListener<CategoryChildBean[]>() {
                     @Override
                     public void onSuccess(CategoryChildBean[] result) {
@@ -85,7 +87,7 @@ public class CategoryFragment extends BaseFragment {
                         if (result != null && result.length > 0) {
                             ArrayList<CategoryChildBean> childList = ConvertUtils.array2List(result);
                             L.e("childList=" + childList.size());
-                            mChildList.add(childList);
+                            mChildList.set(index,childList);
                         }
                         if (groupCount == mGroupList.size()) {
                             mAdapter.initData(mGroupList, mChildList);
