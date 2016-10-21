b721ae5197753d275202c26cc9d2ff32cc36331b
王鑫鹏 <448932081@qq.com>
2016-10-21 15:29:44 +0800

    添加注册的方法

--- app/src/main/java/cn/ucai/fulicenter/activity/RegisterActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/RegisterActivity.java
@@ -1,5 +1,6 @@
 package cn.ucai.fulicenter.activity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.EditText;
@@ -9,6 +10,12 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.Result;
+import cn.ucai.fulicenter.utils.CommonUtils;
+import cn.ucai.fulicenter.utils.L;
+import cn.ucai.fulicenter.utils.MFGT;
+import cn.ucai.fulicenter.utils.NetDao;
+import cn.ucai.fulicenter.utils.OkHttpUtils;
 import cn.ucai.fulicenter.view.DisplayUtils;
 
 import static cn.ucai.fulicenter.R.string.confirm_password_connot_be_empty;
@@ -19,7 +26,7 @@ import static cn.ucai.fulicenter.R.string.two_input_password;
 import static cn.ucai.fulicenter.R.string.user_name_connot_be_empty;
 
 public class RegisterActivity extends BaseActivity {
-
+    private static final String TAG = RegisterActivity.class.getSimpleName();
     @BindView(R.id.username)
     EditText mUsername;
     @BindView(R.id.Nick)
@@ -33,11 +40,13 @@ public class RegisterActivity extends BaseActivity {
     String username ;
     String pwd ;
     String confirm_pwd;
+    RegisterActivity mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
+        mContext = this;
         super.onCreate(savedInstanceState);
     }
 
@@ -57,14 +66,10 @@ public class RegisterActivity extends BaseActivity {
     }
 
     @OnClick(R.id.btnRegister)
-    public void onClick() {
-        register();
-    }
-
-    /**
-     * 注册
-     */
-    private void register() {
+    public void checkedInput() {
+        /**
+         * 验证
+         */
         nick = mNick.getText().toString().trim();
         username = mUsername.getText().toString().trim();
         pwd = mPassword.getText().toString().trim();
@@ -74,7 +79,7 @@ public class RegisterActivity extends BaseActivity {
             mUsername.requestFocus();
             return;
         }
-        else if (!username.matches("[\\w][\\w\\d_]+")){
+        else if (!username.matches("[a-zA-Z]\\w{5,15}")){
             Toast.makeText(this, getResources().getString(illegal_user_name), Toast.LENGTH_SHORT).show();
             mUsername.requestFocus();
             return;
@@ -93,6 +98,38 @@ public class RegisterActivity extends BaseActivity {
             Toast.makeText(this, getResources().getString(two_input_password), Toast.LENGTH_SHORT).show();
             return;
         }
+        register();
+    }
+    /**
+     * 注册
+     */
+    private void register() {
+        final ProgressDialog pd = new ProgressDialog(mContext);
+        pd.setMessage(getResources().getString(R.string.register));
+        pd.show();
+        NetDao.register(mContext, username, nick, pwd, new OkHttpUtils.OnCompleteListener<Result>() {
+            @Override
+            public void onSuccess(Result result) {
+                pd.dismiss();
+                if (result == null) {
+                    CommonUtils.showShortToast(R.string.register_fail);
+                } else {
+                    if (result.isRetMsg()) {
+                        CommonUtils.showLongToast(R.string.register_success);
+                        MFGT.finish(mContext);
+                    } else {
+                        CommonUtils.showLongToast(R.string.register_fail_exists);
+                    }
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                pd.dismiss();
+                CommonUtils.showShortToast(error);
+                L.e(TAG,"register error="+error);
+            }
+        });
 
     }
 }
