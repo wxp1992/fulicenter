795bb223fbc4ca104000e7a1923644546252da80
王鑫鹏 <448932081@qq.com>
2016-10-22 14:12:32 +0800

    增加登录验证的方法，能正常登陆

--- app/src/main/java/cn/ucai/fulicenter/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/LoginActivity.java
@@ -1,7 +1,9 @@
 package cn.ucai.fulicenter.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
@@ -10,20 +12,30 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.Result;
+import cn.ucai.fulicenter.bean.UserAvatar;
+import cn.ucai.fulicenter.utils.CommonUtils;
 import cn.ucai.fulicenter.utils.L;
 import cn.ucai.fulicenter.utils.MFGT;
+import cn.ucai.fulicenter.utils.NetDao;
+import cn.ucai.fulicenter.utils.OkHttpUtils;
+import cn.ucai.fulicenter.utils.ResultUtils;
 
 public class LoginActivity extends BaseActivity {
-
+    private static final String TAG = LoginActivity.class.getSimpleName();
     @BindView(R.id.username)
     EditText mUsername;
     @BindView(R.id.password)
     EditText mPassword;
+    String username;
+    String password;
+    LoginActivity mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        mContext = this;
         super.onCreate(savedInstanceState);
     }
 
@@ -46,6 +58,7 @@ public class LoginActivity extends BaseActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnLogin:
+                checkedInput();
                 break;
             case R.id.btnRegister:
                 MFGT.gotoRegister(this);
@@ -53,14 +66,66 @@ public class LoginActivity extends BaseActivity {
         }
     }
 
+    private void checkedInput() {
+       username = mUsername.getText().toString().trim();
+        password = mPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(username)) {
+            CommonUtils.showLongToast(R.string.user_name_connot_be_empty);
+            mUsername.requestFocus();
+            return;
+        } else if (TextUtils.isEmpty(password)) {
+            CommonUtils.showLongToast(R.string.confirm_password_connot_be_empty);
+            mPassword.requestFocus();
+            return;
+        }
+        login();
+    }
+
+    private void login() {
+       final ProgressDialog pd = new ProgressDialog(mContext);
+        pd.setMessage(getResources().getString(R.string.logining));
+        pd.show();
+        L.e(TAG,"username="+username+",password="+password);
+        NetDao.login(mContext, username, password, new OkHttpUtils.OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+               Result result= ResultUtils.getResultFromJson(s, UserAvatar.class);
+                pd.dismiss();
+                        L.e(TAG, "result=" + result);
+                if (result == null) {
+                    CommonUtils.showLongToast(R.string.login_fail);
+                }else {
+                    if (result.isRetMsg()) {
+                        UserAvatar user = (UserAvatar) result.getRetData();
+                        L.e(TAG,"useravatar"+user);
+                        MFGT.finish(mContext);
+                    } else {
+                        if (result.getRetCode() == I.MSG_LOGIN_UNKNOW_USER) {
+                            CommonUtils.showLongToast(R.string.login_fail_unknow_user);
+                        } else if (result.getRetCode() == I.MSG_LOGIN_ERROR_PASSWORD) {
+                            CommonUtils.showLongToast(R.string.login_fail_error_password);
+                        } else {
+                            CommonUtils.showLongToast(R.string.login_fail);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                pd.dismiss();
+                CommonUtils.showLongToast(error);
+                L.e(TAG, "error=" + error);
+            }
+        });
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        L.e("mUsername====23423423");
         if (resultCode == RESULT_OK && requestCode == I.REQUEST_CODE_REGISTE) {
             String name = data.getStringExtra(I.User.USER_NAME);
             mUsername.setText(name);
-            L.e("mUsername====");
         }
     }
 }
